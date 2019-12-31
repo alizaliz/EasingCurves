06011fe0cbddd4509aad0c22f14027ea4c84d988
Zain Ali <zain_ali8@hotmail.com>
2019-12-31 20:18:08 +1300

    Add basic ease example for LED fade.

---
+++ examples/EaseLED/EaseLED.ino
@@ -0,0 +1,43 @@
+/* 
+ DESCRIPTION
+ ====================
+ Fade LED with sine easing curve.
+ */
+#include <EasingCurves.h>
+
+#define LED_PIN LED_BUILTIN
+
+// Instantiate an EasingCurves object
+EasingCurves ease = EasingCurves(SINE_IN, 0, 254, 5000);
+
+bool forward = true;
+
+void setup()
+{
+  //Setup the LED :
+  pinMode(LED_PIN, OUTPUT);
+  ease.start();
+}
+
+void loop()
+{
+
+  if (ease.isActive())
+  {
+    int level;
+    if (forward)
+    {
+      level = ease.update();
+    }
+    else
+    {
+      level = ease.reverseUpdate();
+    }
+    analogWrite(LED_PIN, level);
+  }
+  else
+  {
+    ease.restart();
+    forward = !forward;
+  }
+}
